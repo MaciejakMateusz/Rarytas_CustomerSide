c68bff00a6a404dcc7679977f189acd1e6f3360c
MaciejakMateusz <maciejakmateusz@gmail.com>
2023-09-10 02:55:02 +0200

    Initial commit containing webapp skeleton: - pom.xml with all needed dependencies - configured application.properties

---
+++ src/main/java/pl/rarytas/rarytas_customerside/RarytasCustomerSideApplication.java
@@ -0,0 +1,13 @@
+package pl.rarytas.rarytas_customerside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RarytasCustomerSideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RarytasCustomerSideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/rarytas/rarytas_customerside/RarytasCustomerSideApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rarytas.rarytas_customerside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RarytasCustomerSideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
